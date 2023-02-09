3fbe621b69e948affc393bb973d7024a66e22c72
David <davidcastroastudillo20@gmail.com>
2023-02-09 18:44:40 -0500

    clases

--- archivo.js
+++ archivo.js
@@ -1,33 +1,19 @@
-class Insumo {
-    constructor(nombre,tiempo){
-        this.nombre = nombre;
-        this.tiempo = tiempo;
+class Sumar {
+    constructor(numero1, numero2){
+        this.numero1 = numero1;
+        this.numero2 = numero2;
     }
-    info() {
-        console.log(`Usted busca un ${this.nombre} que se entregará en ${this.tiempo} días.`)
+
+    valor = (numero1, numero2) => {
+        return numero1 + numero2;
     }
+
 }
 
-class Producto extends Insumo{
-    constructor(nombre,tiempo,pago) {
-    super(nombre,tiempo);
-    this.pago = pago;
-    }
-    infoPago() {
-        console.log(`Su método de pago para el producto ${this.nombre} será con ${this.pago} que se entregará en ${this.tiempo}.`)
-    }
-}
 
-class Entrega extends Producto{
-    constructor(nombre,tiempo,pago,TipoEntrega){
-        super(nombre,tiempo,pago);
-        this.TipoEntrega = TipoEntrega;
-    }
-    infoentrega() {
-        console.log(`Su pedido de ${this.nombre} se entregará en ${this.tiempo} días. Método de pago con ${this.pago} ya confirmado. La forma de entrga será con ${this.TipoEntrega}. Muchas gracias.`)
-    }
-}
 
-const entrega = new Entrega(desinfectante, 5, tarjeta, delivery);
+const clase = new Sumar(1, 2);
 
-Entrega.infoentrega();
+console.log("\n");
+console.log(clase.valor(2, 4));
+console.log("\n");
